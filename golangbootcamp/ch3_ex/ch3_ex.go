a5351d446e046fb730079cd679aee355e9f472e5
Christian Brunotte <christian.brunotte@obi.de>
2020-03-03 13:30:23 +0100

    Exercise from book

---
+++ golangbootcamp/ch3_ex/ch3_ex.go
@@ -0,0 +1,38 @@
+// http://www.golangbootcamp.com/book/types
+package main
+
+import "fmt"
+
+type User struct {
+	Id             int
+	Name, Location string
+}
+
+func (u *User) Greetings() string {
+	return fmt.Sprintf("Hi %s from %s", u.Name, u.Location)
+}
+
+type Player struct {
+	*User
+	GameId int
+}
+
+func (p *Player) String() string {
+	return fmt.Sprintf("Player(Id=%d Name=%s Location=%s GameId=%d)", p.Id, p.Name, p.Location, p.GameId)
+}
+
+func NewPlayer(id int, name string, location string, gameId int) *Player {
+	return &Player{User: &User{Id: id, Name: name, Location: location}, GameId: gameId}
+}
+
+func main() {
+	player1 := Player{
+		User:   &User{Id: 23, Name: "Tim", Location: "Cologne"},
+		GameId: 4,
+	}
+	println(player1.Greetings())
+
+	// Soluton
+	player2 := NewPlayer(42, "Matt", "LA", 90404)
+	println(player2.Greetings())
+}
---
+++ golangbootcamp/ch3_ex/go3_ex_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test1(t *testing.T) {
+	description := "NewPlayer creation"
+
+	var expectedUser = User{
+		Id:       42,
+		Name:     "Matt",
+		Location: "LA",
+	}
+	var expectedPlayer = Player{
+		User:   &expectedUser,
+		GameId: 90404,
+	}
+
+	if actualPlayer := NewPlayer(42, "Matt", "LA", 90404); actualPlayer.String() != expectedPlayer.String() {
+		t.Fatalf("FAIL: %s\nExpected: %s\nActual:   %s", description, expectedPlayer.String(), actualPlayer.String())
+	}
+	t.Logf("PASS: %s", description)
+}
